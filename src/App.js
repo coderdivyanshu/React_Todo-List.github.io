1d7d4622c36c2cc169e105570b2c91e35887ae77
Divyanshu Singh <91016899+divyanshuThecoder123@users.noreply.github.com>
2023-03-04 01:32:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './style.css'
+import Todo from './Todo';
+function App() {
+  return (
+    <div className="App">
+
+     <Todo/>
+    </div>
+  );
+}
+
+export default App;
